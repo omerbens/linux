8922f288c9171f90dce2f83faff6d6f85d5885e5
omer <omer@bensha>
2021-11-10 19:12:12 +0200

    kernel module wip

--- my_module/simple.c
+++ my_module/simple.c
@@ -1,17 +1,30 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/prinfo.h>
+
+typedef int (*ptree_func)(struct prinfo *buf, int *nr, int pid);
+extern int register_ptree(ptree_func func);
+extern void unregister_ptree(ptree_func func);
 
 MODULE_DESCRIPTION ("mine");
 MODULE_LICENSE ("GPL");
 
+int ptree_impl(struct prinfo *buf, int *nr, int pid) {
+	return 0;
+}
+
 static int sample_init(void) {
-	pr_info("module loaded\n");
-        return 0;
+	printk("module loaded\n");
+	register_ptree(&ptree_impl);
+	printk("register function\n");
+	unregister_ptree(&ptree_impl);
+	printk("unregister function\n");
+	return 0;
 }
 
 static void sample_exit(void) {
-        pr_info("module unloaded\n");
+        printk("module unloaded\n");
 }
 
 module_init(sample_init);
