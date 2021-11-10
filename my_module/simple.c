2b1ac0dd08f94e93cad39279ed469faeb7b17260
omer <omer@bensha>
2021-11-10 22:44:56 +0200

    updated getpree impl to dummy one

--- my_module/simple.c
+++ my_module/simple.c
@@ -1,6 +1,8 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/string.h>
+#include <linux/slab.h>
 #include <linux/prinfo.h>
 
 typedef int (*ptree_func)(struct prinfo *buf, int *nr, int pid);
@@ -10,15 +12,31 @@ extern void unregister_ptree(ptree_func func);
 MODULE_DESCRIPTION ("mine");
 MODULE_LICENSE ("GPL");
 
-int ptree_impl(struct prinfo *buf, int *nr, int pid) {
+int getptree(struct prinfo *_buf, int *nr, int pid) {
+	const char *dummpy = "dummy";
+	int i;
+
+	if (NULL == _buf || NULL == nr || 1 > *nr) {
+		return -EINVAL;
+	}
+
+	for (i=0; i<*nr; i++) {
+		struct prinfo *_new = kmalloc(sizeof(struct prinfo), GFP_KERNEL);
+		_new->parent_pid=i;
+		_new->pid=i+1;
+		_new->state=0;
+		_new->uid=0;
+		strcpy(_new->comm, dummpy);
+		_new->level=i;
+	}
 	return 0;
 }
 
 static int sample_init(void) {
 	printk("module loaded\n");
-	register_ptree(&ptree_impl);
+	register_ptree(&getptree);
 	printk("register function\n");
-	unregister_ptree(&ptree_impl);
+	unregister_ptree(&getptree);
 	printk("unregister function\n");
 	return 0;
 }
