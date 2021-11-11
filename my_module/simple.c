151eb68c9f5d65ff87f8e998001629dda914839f
omer <omer@bensha>
2021-11-11 21:40:40 +0200

    wip

--- my_module/simple.c
+++ my_module/simple.c
@@ -4,29 +4,87 @@
 #include <linux/string.h>
 #include <linux/slab.h>
 #include <linux/prinfo.h>
+#include <linux/sched.h>
 
 typedef int (*ptree_func)(struct prinfo *buf, int *nr, int pid);
 extern int register_ptree(ptree_func func);
 extern void unregister_ptree(ptree_func func);
 
+struct task_level {
+	int level;
+	struct task_struct *value;
+};
+
 MODULE_DESCRIPTION ("mine");
 MODULE_LICENSE ("GPL");
 
+void write_data(struct prinfo *buf, struct task_struct *t, int level) {
+	buf->pid = t->pid;
+	buf->parent_pid = t->real_parent->pid;
+	buf->state = t->state;
+	buf->uid=0;
+	strncpy(buf->comm, t->comm, 15);
+	buf->level=level;
+}
+
 int getptree(struct prinfo *buf, int *nr, int pid) {
-	const char *dummpy = "dummy";
+	int count = 1;
+	int current_level = 0;
+	int is_added;
+	struct task_level *bfs;
+	struct task_struct *t, *s;
+	struct list_head *pos;
 	int i;
 
 	if (NULL == buf || NULL == nr || 1 > *nr) {
 		return -EINVAL;
 	}
-	for (i=0; i<*nr; i++) {
-		buf[i].parent_pid=i;
-		buf[i].pid=i+1;
-		buf[i].state=0;
-		buf[i].uid=0;
-		strcpy(buf[i].comm, dummpy);
-		buf[i].level=i;
+	bfs = kmalloc((*nr) * sizeof(struct task_level), GFP_KERNEL);
+
+	rcu_read_lock();
+	t = pid_task(find_vpid(pid), PIDTYPE_PID);
+	if (NULL == t) {
+		rcu_read_unlock();
+		return -EINVAL;
 	}
+	bfs[0].level = 0;
+	bfs[0].value = t;
+
+	// filling bfs
+	while (count < *nr) {
+		is_added = 0;
+		for (i=0; i<count && count <= *nr; i++) {
+			if (bfs[i].level != current_level)
+				continue;
+
+			list_for_each(pos, &t->children) {
+				s = list_entry(pos, struct task_struct, children);
+				bfs[count].level = current_level+1;
+				bfs[count].value = s;
+				count++;
+				is_added = 1;
+
+				if (count >= *nr)
+					break;
+			}
+		}
+		current_level++;
+
+		// no new children
+		if (0 == is_added)
+			break;
+	}
+
+	// update nr
+	*nr = count;
+
+	// writing bfs to buf
+	for (i=0; i<count; i++) {
+		write_data(&buf[i], bfs[i].value, bfs[i].level);
+	}
+	rcu_read_unlock();
+
+	kfree(bfs);
 	return 0;
 }
 
