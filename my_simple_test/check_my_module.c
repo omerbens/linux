9ba4c4b22890b4deaf1cb7984bc88f08b104c392
omer <omer@bensha>
2021-11-28 23:49:54 +0200

    solved test1

--- my_simple_test/check_my_module.c
+++ my_simple_test/check_my_module.c
@@ -1,15 +1,16 @@
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/syscall.h>
 #include <unistd.h>
+#include <sys/syscall.h>
+#include <sys/mman.h>
 
 #define SYSCALL_NUM 449
 #define STARTING_LEN 500
+#define PAGE_SIZE 4096
 
 int syscall_mapspages(unsigned long start, unsigned long end, char *buf, size_t size) {
 	int ret;
-	printf("addresses values are %#lx and %#lx (length is: %#lx)\n", start, end, end-start);
+	printf("addresses values are %#lx and %#lx (length is: %#lx or %ld pages)\n", start, end, end-start, (end-start)/PAGE_SIZE);
         ret = syscall(SYSCALL_NUM, start, end, buf, size);
         if (0 > ret){
                 perror("Failed with");
@@ -19,7 +20,7 @@ int syscall_mapspages(unsigned long start, unsigned long end, char *buf, size_t 
         return ret;
 }
 
-char * increasing_mapspages(unsigned long start, unsigned long end) {
+void print_increasing_mapspages(unsigned long start, unsigned long end) {
 	unsigned int buffer_length = STARTING_LEN;
 	int ret;
 	char *c;
@@ -32,13 +33,14 @@ char * increasing_mapspages(unsigned long start, unsigned long end) {
 			buffer_length *= 2;
 			continue;
 		}
-		return c;
+		printf("output is:\n%s\n\n", c);
+		free(c);
+		return;
 	}
-
+	
 }
 
-int main(int argc, char **argv)
-{
+int cmd_program(int argc, char **argv) {
 	unsigned long start, end;
 	char *c;
 
@@ -50,8 +52,32 @@ int main(int argc, char **argv)
 	start = (unsigned long) strtol(argv[1], NULL, 16);
 	end = (unsigned long) strtol(argv[2], NULL, 16);
 
-	c = increasing_mapspages(start, end);
-	printf("\n\noutput is:\n%s", c);
-	free(c);
+	print_increasing_mapspages(start, end);
 	return 1;
+}
+
+int main() {
+	char *c;
+	unsigned long addr;
+	int len;
+
+	len = 10;
+	addr = (unsigned long) mmap(NULL, PAGE_SIZE*len, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+	if ((void*)addr == MAP_FAILED) {
+		perror("failed mmap");
+		return 1;
+	}
+	print_increasing_mapspages(addr, addr+PAGE_SIZE*len);
+	munmap((void*)addr, PAGE_SIZE*len);
+
+
+
+	len = 10;
+	addr = (unsigned long) mmap(NULL, PAGE_SIZE*len, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS | MAP_LOCKED, -1, 0);
+	if ((void*)addr == MAP_FAILED) {
+		perror("failed mmap");
+		return 1;
+	}
+	print_increasing_mapspages(addr, addr+PAGE_SIZE*len);
+	munmap((void*)addr, PAGE_SIZE*len);
 }
