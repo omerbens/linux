4a3547431aecc5ccc3f78b613e0e074999c579bc
omer <omer@bensha>
2021-11-28 21:18:26 +0200

    updated check program

--- my_simple_test/check_my_module.c
+++ my_simple_test/check_my_module.c
@@ -5,21 +5,42 @@
 #include <unistd.h>
 
 #define SYSCALL_NUM 449
+#define STARTING_LEN 500
 
 int syscall_mapspages(unsigned long start, unsigned long end, char *buf, size_t size) {
 	int ret;
-	printf("value is %#lx %ld\n", start, start);
+	printf("addresses values are %#lx and %#lx (length is: %#lx)\n", start, end, end-start);
         ret = syscall(SYSCALL_NUM, start, end, buf, size);
         if (0 > ret){
                 perror("Failed with");
 		return -1;
 	}
+	printf("return value is %d\n", ret);
         return ret;
+}
+
+char * increasing_mapspages(unsigned long start, unsigned long end) {
+	unsigned int buffer_length = STARTING_LEN;
+	int ret;
+	char *c;
+
+	while (1 == 1) {
+		c = malloc(buffer_length);
+		ret = syscall_mapspages(start, end, c, buffer_length);
+		if (ret == buffer_length) {
+			free(c);
+			buffer_length *= 2;
+			continue;
+		}
+		return c;
+	}
+
 }
 
 int main(int argc, char **argv)
 {
 	unsigned long start, end;
+	char *c;
 
 	if (argc < 3 || argc > 4) {
 		printf("usage: check <start> <end>\n");
@@ -29,10 +50,8 @@ int main(int argc, char **argv)
 	start = (unsigned long) strtol(argv[1], NULL, 16);
 	end = (unsigned long) strtol(argv[2], NULL, 16);
 
-	char *c = malloc(2);
-	printf("syscall end with: %d\n", syscall_mapspages(start, end, c, 2));
-
-	printf("sleeping");
-	sleep(300);
-	printf("done sleeping");
+	c = increasing_mapspages(start, end);
+	printf("\n\noutput is:\n%s", c);
+	free(c);
+	return 1;
 }
